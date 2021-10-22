4eb1eb8626af49216eb675abb2e736af49feb242
Helle Hiorth-Schøyen <helle@solidsquare.no>
2021-10-22 14:30:58 +0200

    storybook cleanup

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,14 @@
+const path = require('path');
+
 module.exports = {
   "stories": [
     "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../**/*.stories.@(js|jsx|ts|tsx)",
+    "../**/*.stories.mdx"
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
   ]
 }
+
