64967fc6f24f81f1904ea2fbf7d7074bc62adc82
Viggo Normann <v@iggo.no>
2022-04-22 14:22:14 +0200

    Export links so that they can be more easily be imported
    Example:
    `import { SolidLink, InvertedLink } from '@posten-hedwig/link`

---
+++ packages/link/src/index.js
@@ -0,0 +1,6 @@
+import Link from './Link'
+import SolidLink from './SolidLink'
+import InvertedLink from './InvertedLink'
+import SimpleLink from './SimpleLink'
+
+export { Link, SolidLink, InvertedLink, SimpleLink }
