2056fff6e9b987dfa4baf2225a77fea35f180747
Viggo Normann <v@iggo.no>
2022-04-26 11:08:41 +0200

    Add type checking to Linklist for Link and <a>

--- packages/linklist/src/Linklist.jsx
+++ packages/linklist/src/Linklist.jsx
@@ -4,47 +4,25 @@ import '../dist/linklist.scss'
 
 export default function Linklist({ links, small }) {
     let classes = ['hw-linklist']
-
+    if (small) classes.push('hw-linklist--small')
     return (
-        <ul className={classes}>
-            {links.map((link) => {
-                return (
-                    <li>
-                        {{
-                            ...link,
-                            key: link.props.href + link.props.children
-                        }}
-                    </li>
-                )
-            })}
+        <ul className={classes.join(' ')}>
+            {links.map((link) => (
+                <li key={link.props.children}>{link}</li>
+            ))}
         </ul>
     )
 }
 
 Linklist.propTypes = {
-    links: PropTypes.arrayOf(function (
-        propValue,
-        key,
-        componentName,
-        location,
-        propFullName
-    ) {
-        console.log(propValue)
-        console.log(key)
-        console.log(componentName)
-        console.log(location)
-        console.log(propFullName)
-        console.log(propValue[key])
-
-        /*
-        if (!/matchme/.test(propValue[key])) {
-          return new Error(
-            'Invalid prop `' + propFullName + '` supplied to' +
-            ' `' + componentName + '`. Validation failed.'
-          );
-        }*/
-        return true
-    }),
+    links: PropTypes.arrayOf(
+        PropTypes.shape({
+            props: PropTypes.shape({
+                href: PropTypes.string.isRequired,
+                children: PropTypes.node.isRequired
+            })
+        })
+    ),
     small: PropTypes.bool
 }
 
