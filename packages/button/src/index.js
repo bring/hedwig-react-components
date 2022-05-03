839ff1f1a17ce06375ff9264857a2363a2952f7b
Viggo Normann <v@iggo.no>
2022-05-03 14:14:15 +0200

    Export Buttons

---
+++ packages/button/src/index.js
@@ -0,0 +1,6 @@
+export { default as ChatButton } from './ChatButton'
+export { default as LightOutlineButton } from './LightOutlineButton'
+export { default as PrimaryButton } from './PrimaryButton'
+export { default as PrimaryOutlineButton } from './PrimaryOutlineButton'
+export { default as SecondaryButton } from './SecondaryButton'
+export { default as SecondaryOutlineButton } from './SecondaryOutlineButton'
