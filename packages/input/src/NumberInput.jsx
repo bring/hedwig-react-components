4e47acdacbd7bd0789145546919f40b8858e7d69
Helle Hiorth-Schøyen <helle@solidsquare.no>
2022-10-19 10:51:14 +0200

    some code cleanup

--- packages/input/src/NumberInput.jsx
+++ packages/input/src/NumberInput.jsx
@@ -2,53 +2,53 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import BaseInput from './BaseInput'
 
-export default function NumberInput({
-  name,
-  id,
-  label,
-  placeholder,
-  min,
-  max,
-  steps,
-  variant,
-  disabled,
-  errorMessage,
-  onChange,
-  readOnly
-}) {
-  return (
+const NumberInput ({
+    name,
+    id,
+    label,
+    placeholder,
+    min,
+    max,
+    steps,
+    variant,
+    disabled,
+    errorMessage,
+    onChange,
+    readOnly
+}) => (
     <BaseInput
-      name={name}
-      id={id}
-      variant={variant}
-      label={label}
-      type='number'
-      min={min}
-      max={max}
-      steps={steps}
-      placeholder={placeholder}
-      disabled={disabled}
-      onChange={onChange}
-      errorMessage={errorMessage}
-      readOnly={readOnly}
+        name={name}
+        id={id}
+        variant={variant}
+        label={label}
+        type='number'
+        min={min}
+        max={max}
+        steps={steps}
+        placeholder={placeholder}
+        disabled={disabled}
+        onChange={onChange}
+        errorMessage={errorMessage}
+        readOnly={readOnly}
     />
-  )
-}
+)
 
 NumberInput.defaultProps = {
-  disabled: false,
+    disabled: false
 }
 
 NumberInput.propTypes = {
-  name:PropTypes.string,
-  id:PropTypes.string,
-  variant: PropTypes.oneOf(['', 'white', 'line']),
-  disabled: PropTypes.bool,
-  readOnly: PropTypes.bool,
-  onChange: PropTypes.func,
-  errorMessage: PropTypes.string,
-  placeholder: PropTypes.string,
-  label: PropTypes.string,
-  min: PropTypes.number,
-  max :PropTypes.number,
+    name: PropTypes.string,
+    id: PropTypes.string,
+    variant: PropTypes.oneOf(['', 'white', 'line']),
+    disabled: PropTypes.bool,
+    readOnly: PropTypes.bool,
+    onChange: PropTypes.func,
+    errorMessage: PropTypes.string,
+    placeholder: PropTypes.string,
+    label: PropTypes.string,
+    min: PropTypes.number,
+    max: PropTypes.number
 }
+
+export default NumberInput
--- packages/input/src/PasswordInput.jsx
+++ packages/input/src/PasswordInput.jsx
@@ -2,43 +2,43 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import BaseInput from './BaseInput'
 
-export default function PasswordInput({
-  name,
-  id,
-  label,
-  placeholder,
-  variant,
-  disabled,
-  errorMessage,
-  onChange
-}) {
-  return (
+const PasswordInput ({
+    name,
+    id,
+    label,
+    placeholder,
+    variant,
+    disabled,
+    errorMessage,
+    onChange
+}) => (
     <BaseInput
-      name={name}
-      id={id}
-      variant={variant}
-      label={label}
-      type='password'
-      placeholder={placeholder}
-      disabled={disabled}
-      onChange={onChange}
-      errorMessage={errorMessage}
+        name={name}
+        id={id}
+        variant={variant}
+        label={label}
+        type='password'
+        placeholder={placeholder}
+        disabled={disabled}
+        onChange={onChange}
+        errorMessage={errorMessage}
     />
-  )
-}
+)
 
 PasswordInput.defaultProps = {
-  disabled: false,
-  type: 'text'
+    disabled: false,
+    type: 'text'
 }
 
 PasswordInput.propTypes = {
-  name:PropTypes.string,
-  id:PropTypes.string,
-  variant: PropTypes.oneOf(['', 'white', 'line']),
-  disabled: PropTypes.bool,
-  onChange: PropTypes.func,
-  errorMessage: PropTypes.string,
-  placeholder: PropTypes.string,
-  label: PropTypes.string
+    name: PropTypes.string,
+    id: PropTypes.string,
+    variant: PropTypes.oneOf(['', 'white', 'line']),
+    disabled: PropTypes.bool,
+    onChange: PropTypes.func,
+    errorMessage: PropTypes.string,
+    placeholder: PropTypes.string,
+    label: PropTypes.string
 }
+
+export default PasswordInput
