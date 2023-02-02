66bf399840f98fd1a38a5348421636ba4c759b3a
Arun visvajeet RS <arun.visvajeet@posten.no>
2023-02-02 17:00:01 +0100

    Add unit test

---
+++ jest.config.base.js
@@ -0,0 +1,24 @@
+module.exports = {
+    roots: ['<rootDir>/src'],
+    transform: {
+        '^.+\\.js$': 'babel-jest'
+    },
+    testRegex: '(/src/.*.(test|spec)).(js?|ts?)$',
+    moduleFileExtensions: ['js', 'jsx'],
+    collectCoverage: false,
+    verbose: true,
+    transform: {
+        '^.+\\.jsx?$': 'babel-jest',
+        '^.+\\.css?$': 'jest-transform-css'
+    },
+    moduleFileExtensions: ['js', 'jsx'],
+    testEnvironment: 'jsdom',
+    globals: {
+        'babel-jest': {
+            presets: [
+                ['@babel/preset-env', { targets: { node: 'current' } }],
+                '@babel/preset-react'
+            ]
+        }
+    }
+}
