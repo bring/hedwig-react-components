9e227e8261a6e5858b179a771a14bfd120fdd4b5
Viggo Normann <v@iggo.no>
2023-02-08 10:18:27 +0100

    Add svg transformer to Logo

--- packages/logo/src/Logo.jsx
+++ packages/logo/src/Logo.jsx
@@ -1,17 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import logoBring from '../logos/logo-bring.svg'
-import logoPosten from '../logos/logo-posten.svg'
+import LogoBring from '../logos/logo-bring.svg'
+import LogoPosten from '../logos/logo-posten.svg'
 import '../sass/logo.scss'
 import '@posten-hedwig/core'
 
-const Logo = ({ logo, title }) => (
-    <img
-        className='hw-logo'
-        src={logo === 'posten' ? logoPosten : logoBring}
-        alt={title}
-    />
-)
+const Logo = ({ logo, title }) =>
+    logo === 'posten' ? <LogoPosten alt={title} /> : <LogoBring alt={title} />
 
 Logo.propTypes = {
     /**
