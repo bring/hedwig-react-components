31cba5e99db40d7b63bec8b4ec9141df0334b7d9
Helle Hiorth-Schøyen <helle@solidsquare.no>
2023-09-21 15:23:42 +0200

    formatting

--- packages/typography/src/Typography.stories.js
+++ packages/typography/src/Typography.stories.js
@@ -29,56 +29,56 @@ export default {
 }
 
 const Template = (args) => <div {...args}>
-<p>
-            Text with <span className='hwc-text-small'>small</span> word
-        </p>
-        <p>
-            Text with <span className='hwc-text-smaller'>smaller</span> word
-        </p>
-        <p>
-            Text with <span className='hwc-text-uppercase'>uppercase</span> word
-        </p>
-        <p>
-            Text with <span className='hwc-text-title'>title</span> word
-        </p>
-        <p style={{ width: '100%' }}>
-            <p className='hwc-text-center'>Centered text</p>
-            <p className='hwc-text-right'>Right aligned text</p>
-        </p>
-        <p>
-            <span className='hwc-text-lead'>Lead text</span>
-        </p>
-        <p>
-            <span className='hwc-text-less-line-height'>Less line height</span>
-        </p>
-        <p>
-            Text with a <span className='hwc-text-bold'>bold</span> word
-        </p>
-        <p>
-            Text with a <span className='hwc-text-medium'>medium</span> word
-        </p>
-        <p>
-            Text with a <span className='hwc-text-regular'>regular</span> word
-        </p>
-        <p>
-            Text with a <span className='hwc-text-light'>light</span> word
-        </p>
-        <p>
-            Text with a <span className='hwc-text-technical'>technical</span>{' '}
-            word
-        </p>
-        <p>
-            Text with a{' '}
-            <span className='hwc-text-technical-title'>technical-title</span>{' '}
-            word
-        </p>
-        <p>
-            Text with a{' '}
-            <span className='hwc-text-caption-title'>caption-title</span> word
-        </p>
-        <p>
-            Text with a <span className='hwc-text-caption'>caption</span> word
-        </p>
+    <p>
+        Text with <span className='hwc-text-small'>small</span> word
+    </p>
+    <p>
+        Text with <span className='hwc-text-smaller'>smaller</span> word
+    </p>
+    <p>
+        Text with <span className='hwc-text-uppercase'>uppercase</span> word
+    </p>
+    <p>
+        Text with <span className='hwc-text-title'>title</span> word
+    </p>
+    <p style={{ width: '100%' }}>
+        <p className='hwc-text-center'>Centered text</p>
+        <p className='hwc-text-right'>Right aligned text</p>
+    </p>
+    <p>
+        <span className='hwc-text-lead'>Lead text</span>
+    </p>
+    <p>
+        <span className='hwc-text-less-line-height'>Less line height</span>
+    </p>
+    <p>
+        Text with a <span className='hwc-text-bold'>bold</span> word
+    </p>
+    <p>
+        Text with a <span className='hwc-text-medium'>medium</span> word
+    </p>
+    <p>
+        Text with a <span className='hwc-text-regular'>regular</span> word
+    </p>
+    <p>
+        Text with a <span className='hwc-text-light'>light</span> word
+    </p>
+    <p>
+        Text with a <span className='hwc-text-technical'>technical</span>{' '}
+        word
+    </p>
+    <p>
+        Text with a{' '}
+        <span className='hwc-text-technical-title'>technical-title</span>{' '}
+        word
+    </p>
+    <p>
+        Text with a{' '}
+        <span className='hwc-text-caption-title'>caption-title</span> word
+    </p>
+    <p>
+        Text with a <span className='hwc-text-caption'>caption</span> word
+    </p>
 
 </div>
 
@@ -125,4 +125,4 @@ const TemplateBring = (args) => (
     </div>
 )
 
-    export const BringHeaders = TemplateBring.bind({})
+export const BringHeaders = TemplateBring.bind({})
