2fb1d56b33de9b06ecbf5acf8708d470104716ac
Helle Hiorth-Schøyen <helle@solidsquare.no>
2023-09-21 09:44:51 +0200

    removed folder not in use

--- packages/textarea/__tests__/textarea.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const textarea = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(textarea(), 'Hello from textarea');
-console.info('textarea tests passed');
