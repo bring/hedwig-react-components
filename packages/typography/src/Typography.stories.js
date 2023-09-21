5dc4479394c8b63ecd365cbc94d2d39d94c34c72
Helle Hiorth-Schøyen <helle@solidsquare.no>
2023-09-21 15:22:45 +0200

    updated docs for typography and base

--- packages/typography/src/Typography.stories.js
+++ packages/typography/src/Typography.stories.js
@@ -1,13 +1,35 @@
 import React from 'react'
+import Readme from './Readme.mdx'
+import { Core } from '@posten-hedwig/core'
+import { Base } from '../../base'
 import { Typography } from './index'
+import {
+    Description,
+    Primary,
+    ArgsTable,
+    Stories,
+    PRIMARY_STORY
+} from '@storybook/addon-docs'
 
 export default {
-    title: 'Base/Typography'
+    title: 'Base/Typography',
+    component: Typography,
+    parameters: {
+        docs: {
+            page: () => (
+                <>
+                    <Readme />
+                    <Description />
+                    <Primary />
+                    <Stories />
+                </>
+            )
+        }
+    }
 }
 
-const Template = (args) => (
-    <>
-        <p>
+const Template = (args) => <div {...args}>
+<p>
             Text with <span className='hwc-text-small'>small</span> word
         </p>
         <p>
@@ -57,7 +79,50 @@ const Template = (args) => (
         <p>
             Text with a <span className='hwc-text-caption'>caption</span> word
         </p>
+
+</div>
+
+export const Default = Template.bind({})
+
+const TemplatePosten = (args) => (
+    <>
+        <h1>H1 title</h1>
+        <h2>H2 title</h2>
+        <h3>H3 title</h3>
+        <h4>H4 title</h4>
+        <h5>H5 title</h5>
+        <h6>H6 title</h6>
+        <p>Headings with sizes different than the tags</p>
+        <h1 className='hwc-h1-display'>.hwc-h1-display</h1>
+        <h1 className='hwc-h1'>.hwc-h1</h1>
+        <h1 className='hwc-h2'>.hwc-h2</h1>
+        <h1 className='hwc-h3'>.hwc-h3</h1>
+        <h1 className='hwc-h4'>.hwc-h4</h1>
+        <h1 className='hwc-h5'>.hwc-h5</h1>
+        <h1 className='hwc-h6'>.hwc-h6</h1>
     </>
 )
 
-export const Other = Template.bind({})
+export const PostenHeaders = TemplatePosten.bind({})
+
+
+const TemplateBring = (args) => (
+    <div className='hwc-theme-bring'>
+        <h1>H1 title</h1>
+        <h2>H2 title</h2>
+        <h3>H3 title</h3>
+        <h4>H4 title</h4>
+        <h5>H5 title</h5>
+        <h6>H6 title</h6>
+        <p>Headings with sizes different than the tags:</p>
+        <h1 className='hwc-h1-display'>.hwc-h1-display</h1>
+        <h1 className='hwc-h1'>.hwc-h1</h1>
+        <h1 className='hwc-h2'>.hwc-h2</h1>
+        <h1 className='hwc-h3'>.hwc-h3</h1>
+        <h1 className='hwc-h4'>.hwc-h4</h1>
+        <h1 className='hwc-h5'>.hwc-h5</h1>
+        <h1 className='hwc-h6'>.hwc-h6</h1>
+    </div>
+)
+
+    export const BringHeaders = TemplateBring.bind({})
